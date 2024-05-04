295dfe695f84a61c7bbe90732241c9bf8e8ddcf9
dominicmacaulay <dominic.62502@gmail.com>
2024-05-04 10:53:19 -0400

    Add initial game play methods

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -2,8 +2,10 @@ class Game
     def initialize
         @error_count = 6
         @word = select_word
-        @board = Board.new(@error_count, @word)
-        puts @board.display_board
+        @guess_slots = create_slots
+        @board = Board.new(@error_count, @word, @guess_slots)
+        @board.display_board
+        play_game
     end
 
     puts "Hangman initializing..."
@@ -14,27 +16,60 @@ class Game
         until word.length >= 5 && word.length <= 12
             word = word_pool.sample.chomp
         end
+        puts word
         word
     end
+
+    def create_slots
+        slots = []
+        until slots.length == @word.length
+            slots.push('_')
+        end
+        slots
+    end
     
+    def play_game
+        guess = get_input
+    end
+
+    def get_input
+        puts "Guess a letter"
+        guess = gets.chomp.upcase
+        until guess.length == 1
+            puts "Error: type a single character to guess"
+            guess = gets.chomp.upcase
+        end
+        guess
+    end
+
+    def check_guess(guess)
+        unless @word.inlcude? guess
+            @error_count -= 1
+            @game.display_incorrect(guess)
+        else
+            update_slots(guess)
+        end
+        @game.display_board
+    end
+
+    def update_slots(guess)
+    end
 end
 
 class Board
-    def initialize(count, word)
+    def initialize(count, word, slots)
         @count = count
         @word = word
-        @guess_slots = []
-        create_display
-    end
-
-    def create_display
-        until @guess_slots.length == @word.length
-            @guess_slots.push('_')
-        end
+        @guess_slots = slots
     end
 
     def display_board
-        @guess_slots.join(' ')
+        puts "Incorrect guesses left: #{@count}"
+        puts @guess_slots.join(' ')
+    end
+
+    def display_incorrect(guess)
+        puts "The letter '#{guess}' is not in this word"
     end
 end
 
