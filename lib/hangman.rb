faba841e38e3a8c282285ba2f1b6e0fb887a931b
dominicmacaulay <dominic.62502@gmail.com>
2024-05-04 09:50:08 -0400

    Add select word method

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,3 +1,12 @@
 puts "Hangman initializing..."
 
-word_pool = File.read('word_dictionary.txt')
+def select_word
+    word_pool = File.readlines('word_dictionary.txt')
+    word = ''
+    until word.length >= 5 && word.length <= 12
+        word = word_pool.sample.chomp
+    end
+    word
+end
+
+puts select_word
